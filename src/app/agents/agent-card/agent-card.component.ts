a6efb07f368f9510b12144f2b73dd7759b5b0805
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2025-01-13 10:39:11 +0100

    feat: :sparkles: navigate on click

--- src/app/agents/agent-card/agent-card.component.ts
+++ src/app/agents/agent-card/agent-card.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Agents} from '../agents.model';
 import {NgOptimizedImage} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-agent-card',
@@ -12,4 +13,15 @@ import {NgOptimizedImage} from '@angular/common';
 })
 export class AgentCardComponent {
   @Input() agent: Agents = {} as Agents;
+
+  constructor(private readonly router: Router) {
+  }
+
+  onCardClick() {
+    this.router.navigate(['/agent/', this.agent.id])
+      .then(() => {},
+        error => {
+        console.error(`Error navigating to agent ${this.agent.id}, error: ${error}`);
+        });
+  };
 }
---
+++ src/app/agents/agent-overview/agent-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-overview',
+  imports: [],
+  templateUrl: './agent-overview.component.html',
+  styleUrl: './agent-overview.component.css'
+})
+export class AgentOverviewComponent {
+
+}

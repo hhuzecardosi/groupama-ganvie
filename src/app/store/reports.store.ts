5e388ab18e7449996984bc4f4fc7543517c96d0e
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2025-01-14 16:27:55 +0100

    fix: :bug: store

--- src/app/store/reports.store.ts
+++ src/app/store/reports.store.ts
@@ -1,4 +1,4 @@
-import {patchState, signalStore, type, withComputed, withMethods, withState} from '@ngrx/signals';
+import {patchState, signalStore, type, withComputed, withHooks, withMethods, withState} from '@ngrx/signals';
 
 import {Reports} from '../reports/reports.model';
 import {computed, inject} from '@angular/core';
@@ -28,21 +28,15 @@ export const ReportsStore = signalStore(
     sortReports: computed(() => reports().sort((a, b) => a.date.localeCompare(b.date)))
   })),
   withMethods((store, reportService = inject(MockReportsService)) => ({
-   /*
-   * Methods to add
-   * 1. Get reports list (id/date) by mission and agent
-   * 2. Get a specific report by its id
-   * 3. Add a report to the store
-   * */
     getReportsListByAgentAndMission(agentId: string, missionId: string): Observable<{ id: string, date: string }[]> {
       return reportService.getReports()
         .pipe(map(reports => {
-          const storeReports = store.reports();
-          const filteredReports = reports.filter(report => report.agentId === agentId && report.missionId === missionId);
+          const storeReports = store.reports()
+          const storeIds = storeReports.map(report => report.id);
 
-          let ids = storeReports.map(report => report.id);
-          patchState(store, state => ({...state, reports: [...storeReports, ...filteredReports.filter(report => !ids.includes(report.id))]}))
-          return store.listReports().sort((a, b) => a.date.localeCompare(b.date));
+          patchState(store, state => ({...state, reports: [...storeReports, ...reports.filter(report => !storeIds.includes(report.id))]}));
+          return store.reports().filter(report => report.agentId === agentId && report.missionId === missionId)
+            .map(report => ({id: report.id, date: report.date}));
         }));
     },
     getReportById(id: string): Observable<Reports | undefined> {
@@ -56,6 +50,15 @@ export const ReportsStore = signalStore(
     },
     addReport(report: Reports): void {
       patchState(store, state => ({...state, reports: [...state.reports, report]}));
+    },
+  })),
+  withHooks((store, reportService = inject(MockReportsService)) => ({
+    onInit() {
+      patchState(store, {isLoading: true});
+      reportService.getReports()
+        .subscribe((reports) => {
+          patchState(store, {reports, isLoading: false, initialized: true});
+        });
     },
   }))
 );
